5930be7f807fcd7fc09f4385f0be3f7e19fbc80a
Russell Gordon <rgordon@lcs.on.ca>
2024-06-13 09:39:29 -0400

    Initial Commit

---
+++ ListWithChildren/ListWithChildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithChildrenApp.swift
+//  ListWithChildren
+//
+//  Created by Russell Gordon on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct ListWithChildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
